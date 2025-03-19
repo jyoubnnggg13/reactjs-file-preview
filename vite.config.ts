a245b3aa1d6e138a97e7f08aefd4ab92efc32878
jaditi930 <jaditi930@gmail.com>
2025-03-19 23:49:45 +0530

    added inject css plugin in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
 
 export default defineConfig({
-  plugins: [react(),dts()],
+  plugins: [react(),dts(),cssInjectedByJsPlugin()],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.tsx"),
